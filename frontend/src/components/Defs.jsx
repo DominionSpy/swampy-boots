d58ec71c74ab8f348cd74db8a5a5da7e4d16d328
DominionSpy <dominionspy@hotmail.com>
2026-03-16 06:06:47 +0200

    Remove unused defs

--- frontend/src/components/Defs.jsx
+++ frontend/src/components/Defs.jsx
@@ -1,21 +1,14 @@
 import { getFillStyle } from '../utils/style.js'
 
 const Defs = ({ panel }) => {
-  const lw = panel.style.lineWidth
-  const hw = lw / 2
+  const lineWidth = panel.style.lineWidth
 
   const gridStyle = getFillStyle(panel.style.gridColor)
 
   return (
     <defs>
-      <g id={'deadend' + panel.id}>
-        <rect x={-hw} y={-hw} width={lw} height={lw} style={gridStyle} />
-      </g>
-      <g id={'corner' + panel.id}>
-        <circle r={hw} style={gridStyle} />
-      </g>
       <g id={'start' + panel.id}>
-        <circle r={lw} style={gridStyle} />
+        <circle r={lineWidth} style={gridStyle} />
       </g>
       <g id={'block' + panel.id}>
         <rect x='-0.4' y='-0.4' width='0.8' height='0.8' />
