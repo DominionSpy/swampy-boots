e663ee6173422a51efa690a7518d02d29edf70f1
DominionSpy <dominionspy@hotmail.com>
2026-03-20 21:35:49 +0200

    Address PR comments

--- frontend/src/components/Thumbnail.jsx
+++ frontend/src/components/Thumbnail.jsx
@@ -1,38 +1,19 @@
+import thumbnail from './thumbnail.module.css'
 import { Link } from 'react-router-dom'
 
 const Thumbnail = ({ width, height, title, href = '/', children }) => {
-  const containerStyle = {
-    float: 'left',
-    margin: 2,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#5f5f5f',
-    borderRadius: 10,
-    backgroundColor: '#1f1f1f'
-  }
-
-  const headerStyle = {
-    width: width + 'px',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    backgroundColor: '#3f3f3f',
-    color: 'white'
-  }
-
-  const placeholderStyle = {
-    width: width + 'px',
-    height: height + 'px',
-    padding: 10
+  const variableStyle = {
+    '--thumbnail-width': width + 'px',
+    '--thumbnail-height': height + 'px',
   }
 
   return (
-    <Link to={href} style={containerStyle}>
-      <div style={headerStyle}>
+    <Link to={href} className={thumbnail.container}
+      style={variableStyle}>
+      <div className={thumbnail.header}>
         {title}
       </div>
-      <div style={placeholderStyle}>
+      <div className={thumbnail.placeholder}>
         {children}
       </div>
     </Link>
