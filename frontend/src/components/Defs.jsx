66657553f58be441b1ce2f8937f5f571383f40ff
DominionSpy <dominionspy@hotmail.com>
2026-03-23 08:56:11 +0200

    Use renderPolygon

--- frontend/src/components/Defs.jsx
+++ frontend/src/components/Defs.jsx
@@ -1,12 +1,12 @@
 import { getFillStyle } from '../utils/style'
-import { renderStar, renderFlatStar } from '../utils/svg'
+import { renderPolygon, renderStar, renderFlatStar } from '../utils/svg'
 
 const Defs = ({ panel }) => {
   const lineWidth = panel.style.lineWidth
 
   const gridStyle = getFillStyle(panel.style.gridColor)
 
-  const pillPath = 'M-0.225,0,-0.113,-0.195,0.113,-0.195,0.225,0,0.113,0.195,-0.113,0.195Z'
+  const pillPath = renderPolygon(6, 0.25)
   const hollowPath = 'M-0.4,-0.4L0.4,-0.4L0.4,0.4L-0.4,0.4Z'
     + 'M-0.2,-0.2L-0.2,0.2L0.2,0.2L0.2,-0.2Z'
   const hollowStyle = getFillStyle('blue')
@@ -16,7 +16,7 @@ const Defs = ({ panel }) => {
   return (
     <defs>
       <circle id={`start${panel.id}`} r={lineWidth} style={gridStyle} />
-      <path id={`pill${panel.id}`} d={pillPath} />
+      <path id={`pill${panel.id}`} d={pillPath} transform='rotate(90)' />
       <rect id={`stone${panel.id}`} x='-0.3' y='-0.3' width='0.6' height='0.6' rx='0.16' />
       <rect id={`block${panel.id}`} x='-0.4' y='-0.4' width='0.8' height='0.8' />
       <path id={`hollow${panel.id}`} d={hollowPath} style={hollowStyle} />
